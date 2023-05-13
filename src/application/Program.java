88b73ceccd2fc31c660725394d3615a14bf44dc1
Yan Velasco <yancarlosduartevelasco@gmail.com>
2023-05-13 16:12:19 -0300

    Add loop for contracts

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,21 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Department;
+import entities.HourContract;
 import entities.Worker;
 import entities.enums.WorkerLevel;
 
 public class Program {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args, HourContract HourContract) throws Exception {
         Locale.setDefault(Locale.US);
         Scanner entry = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+
         System.out.println("Enter departament`s name: "); 
         String departament = entry.nextLine();
         System.out.println("Enter work data: ");
@@ -21,10 +26,24 @@ public class Program {
         System.err.println("Base salary: ");
         double baseSalary = entry.nextDouble();
 
-        Worker work = new Worker(workerName, baseSalary, WorkerLevel.valueOf(workerLevel), new Department(departament));
-        System.out.println(work);
+        Worker worker = new Worker(workerName, baseSalary, WorkerLevel.valueOf(workerLevel), new Department(departament));
 
+        System.err.println("How many contracts to this worker? ");
+        int n = entry.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            System.out.println("Enter contract #" + i + " data: ");
+            System.out.println("Date (DD/MM/YYYY): ");
+            Date contractDate = sdf.parse(entry.next());
+            System.err.println("Value per hour: ");
+            double valuePerHour = entry.nextDouble();
+            System.out.println("Duration (hours): ");
+            int hours = entry.nextInt();
+            HourContract contract= new HourContract(contractDate, valuePerHour, hours);
+            worker.addContract(contract);
+        }
 
         entry.close();
     }
 }
+
